d60240843e9a75028eaf242a48c8c0f88389a7be
Thanh Duy Trinh <thanhtrinh501@Thanhs-MacBook-Pro-2.local>
2024-08-28 12:23:44 -0700

    change homepage

--- app/page.js
+++ app/page.js
@@ -8,24 +8,10 @@ import { useEffect, useState } from 'react';
 export default function Home() {
   const {isLoaded, isSigned, user} = useUser();
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const search = searchParams.get('id');
-  const [redirect, setRedirect] = useState(false);
-  useEffect(() => {
-    if (isLoaded) {
-      if (user && search) {
-        // Redirect if user is present
-        router.replace(`/home?id=${search}`);
-        setRedirect(true);
-      } else{
-        // Redirect if there's an id in the search params
-        router.replace(`/home`);
-        setRedirect(true);
-      }
-      console.log(search);
-    }
-  }, [isLoaded, user, search, router, setRedirect]);
-  if (redirect) return null;
+  if(user){
+    router.push('/home');
+  }
+  
   
   return (
     <>
